efe9d24abc3bb00f5a086c11248dc3e16a15a13f
lonelyjohner <lonelyjohner@163.com>
2015-04-04 20:23:49 -0400

    add cross-validation and multi-classification

--- naive_bayes/naiveBayes_multiclass.py
+++ naive_bayes/naiveBayes_multiclass.py
@@ -1,27 +1,29 @@
 import glob
-from sklearn import linear_model
-from sklearn import naive_bayes
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.multiclass import OneVsRestClassifier
-
+from sklearn.naive_bayes import BernoulliNB
+from sklearn import cross_validation
 file_list = glob.glob("../untag_webkb/*/*/*/*")
 categories = ['course', 'department', 'faculty', 'other', 'project', 'staff', 'student']
 vectorizer = CountVectorizer(input='filename', stop_words='english', encoding='latin1')
 y = []
 
 for file_path in file_list:
-	y.append(file_path.split('/')[2])
+	y.append(file_path.split('/')[3])
 
 file_train, file_test, y_train, y_test = train_test_split(file_list, y, test_size=0.3)
 
 x_train = vectorizer.fit_transform(file_train)
-multiclass_logistic_regression = OneVsRestClassifier(linear_model.LogisticRegression())
-
-multiclass_logistic_regression.fit(x_train, y_train)
+clf = BernoulliNB()
+clf.fit(x_train,y_train)
+#multiclass_logistic_regression.fit(x_train, y_train)
 
 # start testing
+
+
+# scores = cross_validation.cross_val_score(clf, x_train, y_train, cv=5)
+# print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 x_test = vectorizer.transform(file_test)
-y_hat = multiclass_logistic_regression.predict(x_test)
+y_hat = clf.predict(x_test)
 print accuracy_score(y_test,y_hat)
