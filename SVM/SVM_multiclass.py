8320054243ac0814cbf54410b510793520a173be
lonelyjohner <lonelyjohner@163.com>
2015-04-05 16:25:43 -0400

    modified SVM and NB

--- SVM/SVM_multiclass.py
+++ SVM/SVM_multiclass.py
@@ -3,25 +3,30 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+from sklearn import cross_validation
 
-def SVM()
 file_list = glob.glob("../untag_webkb/*/*/*/*")
 categories = ['course', 'department', 'faculty', 'other', 'project', 'staff', 'student']
 vectorizer = CountVectorizer(input='filename', stop_words='english', encoding='latin1')
 y = []
 
 for file_path in file_list:
-	y.append(file_path.split('/')[3])
+    y.append(file_path.split('/')[3])
 
 file_train, file_test, y_train, y_test = train_test_split(file_list, y, test_size=0.3)
 
 x_train = vectorizer.fit_transform(file_train)
-clf = SVC(kernel='linear')
-clf.fit(x_train,y_train)
-#multiclass_logistic_regression.fit(x_train, y_train)
+    #clf = SVC(kernel=ker)
+clf = SVC(kernel='sigmoid')
+    #clf.fit(x_train,y_train)
+
+scores = cross_validation.cross_val_score(clf, x_train, y_train, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+
 
 # start testing
-x_test = vectorizer.transform(file_test)
-y_hat = clf.predict(x_test)
-print accuracy_score(y_test,y_hat)
+# x_test = vectorizer.transform(file_test)
+# y_hat = clf.predict(x_test)
+# print accuracy_score(y_test,y_hat)
 
--- naive_bayes/naiveBayes_multiclass.py
+++ naive_bayes/naiveBayes_multiclass.py
@@ -16,14 +16,14 @@ file_train, file_test, y_train, y_test = train_test_split(file_list, y, test_siz
 
 x_train = vectorizer.fit_transform(file_train)
 clf = BernoulliNB()
-clf.fit(x_train,y_train)
+#clf.fit(x_train,y_train)
 #multiclass_logistic_regression.fit(x_train, y_train)
 
 # start testing
 
 
-# scores = cross_validation.cross_val_score(clf, x_train, y_train, cv=5)
-# print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-x_test = vectorizer.transform(file_test)
-y_hat = clf.predict(x_test)
-print accuracy_score(y_test,y_hat)
+scores = cross_validation.cross_val_score(clf, x_train, y_train, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+# x_test = vectorizer.transform(file_test)
+# y_hat = clf.predict(x_test)
+# print accuracy_score(y_test,y_hat)
